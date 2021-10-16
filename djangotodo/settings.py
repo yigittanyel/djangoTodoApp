ec1b632cc59ba5387c95a9dbfea9e24e33b8d5e3
Yigit <tanyelyigit@gmail.com>
2021-10-16 09:16:47 +0300

    update for settings file

--- djangotodo/settings.py
+++ djangotodo/settings.py
@@ -19,10 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^5$fgbv$!pp#9)+9cn@ph&$f8j^g1ds^l^7l=oql9=b-8mq7ma'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
